b06943164965f032126e296562e9ba37e909b953
Marvin van Aalst <marvin.vanaalst@gmail.com>
2025-09-24 17:35:57 +0200

    dev: fix: relative url

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -2,4 +2,4 @@ import { dev } from '$app/environment'
 
 export const title = 'PicoSvelte'
 export const description = 'PicoSvelte'
-export const url = dev ? 'http://localhost:5173/' : 'https://computational-biology-aachen.github.io/'
+export const url = dev ? 'http://localhost:5173/' : 'https://computational-biology-aachen.github.io/2026-photosynthesis-hackathon/'
