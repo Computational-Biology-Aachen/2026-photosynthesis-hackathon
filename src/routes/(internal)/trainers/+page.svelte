4b425e119eaa182969f1333a15ad85e3f3e60d98
Anna Matuszynska <abmatuszynska@gmail.com>
2025-10-15 16:26:55 +0300

    Update +page.svelte
    Added Dave bio

--- src/routes/(internal)/trainers/+page.svelte
+++ src/routes/(internal)/trainers/+page.svelte
@@ -48,6 +48,21 @@
 			</div>
 		</div>
 	</Section>
+	<div class="row">
+			<img
+				src="{base}/dave.png"
+				alt="Prof. Dr. David Krammer" />
+			<div>
+				<H1 color="primary">Prof. Dr. David Kramer</H1>
+				<H4 color="secondary">Founding Scientific Director</H4>
+
+				<H4 color="secondary">Jan IngenHousz Institute (JII)</H4>
+				<Text>
+					David M. Kramer is a leading researcher in photosynthesis, recognized for his pioneering work on its mechanisms and for developing innovative plant phenotyping tools now widely used to study plant performance and environmental responses. He earned his Ph.D. in Biophysics from the University of Illinois and completed a postdoctoral fellowship at the Institut de Biologie Physico-Chimique in Paris. He then held several faculty positions, including Professor at the Institute for Biological Chemistry at Washington State University, before becoming the Hannah Distinguished Professor in Photosynthesis and Bioenergetics at Michigan State University and Special Professor of Integrative Photosynthesis at Wageningen University. In 2023, he was appointed Founding Scientific Director of the Jan IngenHousz Institute in the Netherlands, where he leads an international effort to advance photosynthesis research and sustainable crop improvement through innovations in open science platforms.
+				</Text>
+			</div>
+		</div>
+	</Section>
 	<Section color="secondary">
 		<div class="row">
 			<img
